0e2edc6f0d5eed94c43fc21231e7306d8a5e5fd9
mstream <maciej.laciak@gmail.com>
2023-04-14 12:17:02 +0200

    refactor: extract canvas into a separate web app component

---
+++ src/MLogo/WebApp/CanvasComponent.purs
@@ -0,0 +1,130 @@
+module MLogo.WebApp.CanvasComponent where
+
+import Prelude
+
+import Control.Monad.State (put)
+import Data.Array as Array
+import Data.List (List)
+import Data.List as List
+import Data.Maybe (Maybe(..))
+import Effect.Aff.Class (class MonadAff)
+import Halogen (Component, ComponentHTML, HalogenM)
+import Halogen as H
+import Halogen.HTML (ClassName(..), HTML)
+import Halogen.Svg.Attributes (Color(..), Transform(..))
+import Halogen.Svg.Attributes as SA
+import Halogen.Svg.Elements as SE
+import MLogo.Interpretation.State
+  ( Angle(..)
+  , PointerState
+  , Position(..)
+  , ScreenState
+  , VisibleState
+  )
+
+data Action = Receive Input
+
+type Input = VisibleState
+
+type State = Input
+
+component ∷ ∀ m o q. MonadAff m ⇒ Component q Input o m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval
+        { handleAction = handleAction
+        , receive = Just <<< Receive
+        }
+    }
+
+initialState ∷ Input → State
+initialState = identity
+
+render ∷ ∀ m. State → ComponentHTML Action () m
+render state =
+  SE.svg
+    [ SA.classes [ ClassName "canvas" ]
+    , SA.viewBox 0.0 0.0 canvasSize canvasSize
+    ]
+    (Array.fromFoldable $ renderVisibleState state)
+
+renderVisibleState ∷ ∀ i w. VisibleState → List (HTML w i)
+renderVisibleState state =
+  renderScreenState
+    state.screen <> renderPointerState state.pointer
+
+renderPointerState ∷ ∀ i w. PointerState → List (HTML w i)
+renderPointerState pointer =
+  let
+    (Position p1) = pointer.position + Position
+      { x: -halfOfPointerSize, y: -halfOfPointerSize }
+    (Position p2) = pointer.position + Position
+      { x: halfOfPointerSize, y: -halfOfPointerSize }
+    (Position p3) = pointer.position + Position
+      { x: zero, y: halfOfPointerSize }
+    (Position p) = pointer.position
+    (Angle a) = pointer.angle
+    stroke = SA.stroke $ Named "green"
+    transform = SA.transform $ transforms <> [ Rotate (-a) p.x p.y ]
+  in
+    List.fromFoldable
+      [ SE.line
+          [ SA.x1 p1.x
+          , SA.y1 p1.y
+          , SA.x2 p2.x
+          , SA.y2 p2.y
+          , stroke
+          , transform
+          ]
+      , SE.line
+          [ SA.x1 p2.x
+          , SA.y1 p2.y
+          , SA.x2 p3.x
+          , SA.y2 p3.y
+          , stroke
+          , transform
+          ]
+      , SE.line
+          [ SA.x1 p3.x
+          , SA.y1 p3.y
+          , SA.x2 p1.x
+          , SA.y2 p1.y
+          , stroke
+          , transform
+          ]
+      ]
+
+renderScreenState ∷ ∀ i w. ScreenState → List (HTML w i)
+renderScreenState = map \{ p1: (Position start), p2: (Position end) } →
+  SE.line
+    [ SA.x1 start.x
+    , SA.y1 start.y
+    , SA.x2 end.x
+    , SA.y2 end.y
+    , SA.stroke $ Named "black"
+    , SA.transform transforms
+    ]
+
+handleAction
+  ∷ ∀ m o. MonadAff m ⇒ Action → HalogenM State Action () o m Unit
+handleAction = case _ of
+  Receive visibleState →
+    put visibleState
+
+canvasSize ∷ Number
+canvasSize = 100.0
+
+transforms ∷ Array Transform
+transforms =
+  [ Translate (canvasSize / 2.0) (canvasSize / 2.0)
+  , Scale one (-one)
+  ]
+
+pointerSize ∷ Number
+pointerSize = 8.0
+
+halfOfPointerSize ∷ Number
+halfOfPointerSize = pointerSize / 2.0
+
--- src/Main/WebApp.purs
+++ src/Main/WebApp.purs
@@ -2,36 +2,23 @@ module Main.WebApp (main) where
 
 import Prelude
 
-import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Foldable (for_)
-import Data.List (List)
-import Data.List as List
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Aff.Class (class MonadAff)
 import Halogen (ClassName(..), Component, Slot)
 import Halogen.Aff (selectElement)
-import Halogen.HTML (HTML)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import Halogen.Hooks as Hooks
-import Halogen.Svg.Attributes (Color(..), Transform(..))
-import Halogen.Svg.Attributes as SA
-import Halogen.Svg.Elements as SE
 import Halogen.VDom.Driver (runUI)
 import MLogo.Interpretation.Command as Command
-import MLogo.Interpretation.State
-  ( Angle(..)
-  , PointerState
-  , Position(..)
-  , ScreenState
-  , VisibleState
-  )
 import MLogo.Program as Program
 import MLogo.WebApp.AceComponent (Output(..))
 import MLogo.WebApp.AceComponent as AceComponent
+import MLogo.WebApp.CanvasComponent as CanvasComponent
 import MLogo.WebApp.LegendComponent as LegendComponent
 import Type.Proxy (Proxy(..))
 import Web.DOM.ParentNode (QuerySelector(..))
@@ -40,10 +27,12 @@ type State = { text ∷ String }
 
 type ChildSlots =
   ( ace ∷ AceComponent.Slot Unit
+  , canvas ∷ ∀ q slot. Slot q Void slot
   , legend ∷ ∀ q slot. Slot q Void slot
   )
 
 _ace = Proxy ∷ Proxy "ace"
+_canvas = Proxy ∷ Proxy "canvas"
 _legend = Proxy ∷ Proxy "legend"
 
 main ∷ Effect Unit
@@ -51,21 +40,6 @@ main = launchAff_ do
   mbDivElem ← selectElement $ QuerySelector "#halogen"
   for_ mbDivElem \divElem → do
     void $ runUI rootComp 0 divElem
-
-canvasSize ∷ Number
-canvasSize = 100.0
-
-transforms ∷ Array Transform
-transforms =
-  [ Translate (canvasSize / 2.0) (canvasSize / 2.0)
-  , Scale one (-one)
-  ]
-
-pointerSize ∷ Number
-pointerSize = 8.0
-
-halfOfPointerSize ∷ Number
-halfOfPointerSize = pointerSize / 2.0
 
 rootComp ∷ ∀ i m o q. MonadAff m ⇒ Component q i o m
 rootComp = Hooks.component \_ _ → Hooks.do
@@ -95,72 +69,11 @@ rootComp = Hooks.component \_ _ → Hooks.do
             HH.div
               [ HP.classes [ ClassName "error" ] ]
               [ HH.text errorMessage ]
-          Right state →
-            renderSvg state
+          Right visibleState →
+            HH.slot_
+              _canvas
+              unit
+              CanvasComponent.component
+              visibleState
       ]
-
-renderSvg ∷ ∀ i w. VisibleState → HTML w i
-renderSvg state = SE.svg
-  [ SA.classes
-      [ ClassName "canvas" ]
-  , SA.viewBox 0.0 0.0 canvasSize canvasSize
-  ]
-  (Array.fromFoldable $ renderState state)
-
-renderState ∷ ∀ i w. VisibleState → List (HTML w i)
-renderState state =
-  renderScreenState
-    state.screen <> renderPointerState state.pointer
-
-renderPointerState ∷ ∀ i w. PointerState → List (HTML w i)
-renderPointerState pointer =
-  let
-    (Position p1) = pointer.position + Position
-      { x: -halfOfPointerSize, y: -halfOfPointerSize }
-    (Position p2) = pointer.position + Position
-      { x: halfOfPointerSize, y: -halfOfPointerSize }
-    (Position p3) = pointer.position + Position
-      { x: zero, y: halfOfPointerSize }
-    (Position p) = pointer.position
-    (Angle a) = pointer.angle
-    stroke = SA.stroke $ Named "green"
-    transform = SA.transform $ transforms <> [ Rotate (-a) p.x p.y ]
-  in
-    List.fromFoldable
-      [ SE.line
-          [ SA.x1 p1.x
-          , SA.y1 p1.y
-          , SA.x2 p2.x
-          , SA.y2 p2.y
-          , stroke
-          , transform
-          ]
-      , SE.line
-          [ SA.x1 p2.x
-          , SA.y1 p2.y
-          , SA.x2 p3.x
-          , SA.y2 p3.y
-          , stroke
-          , transform
-          ]
-      , SE.line
-          [ SA.x1 p3.x
-          , SA.y1 p3.y
-          , SA.x2 p1.x
-          , SA.y2 p1.y
-          , stroke
-          , transform
-          ]
-      ]
-
-renderScreenState ∷ ∀ i w. ScreenState → List (HTML w i)
-renderScreenState = map \{ p1: (Position start), p2: (Position end) } →
-  SE.line
-    [ SA.x1 start.x
-    , SA.y1 start.y
-    , SA.x2 end.x
-    , SA.y2 end.y
-    , SA.stroke $ Named "black"
-    , SA.transform transforms
-    ]
 
