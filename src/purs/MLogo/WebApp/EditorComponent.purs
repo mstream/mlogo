17265adf85db3ada86c6ccf6fc018ae119e1e593
mstream <maciej.laciak@gmail.com>
2023-05-14 13:20:07 +0100

    fix: disable AST update optimization until a proper fixed is found

--- src/purs/MLogo/WebApp/EditorComponent.purs
+++ src/purs/MLogo/WebApp/EditorComponent.purs
@@ -7,13 +7,11 @@ import Ace as Ace
 import Ace.EditSession as Session
 import Ace.Editor as Editor
 import Data.Either (Either(..))
-import Data.List (List(..))
+import Data.List (List)
 import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
-import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
 import Effect.Class (liftEffect)
-import Effect.Class.Console (info)
 import Effect.Exception (throw)
 import Halogen (RefLabel(..), SubscriptionId)
 import Halogen as H
@@ -39,6 +37,8 @@ data Query a
   = SetAst (List Expression) a
   | SetSource String a
 
+type State = { editor ∷ Editor, sourceInfo ∷ SourceInfo }
+
 data SourceInfo
   = Parsable ParsableSourceInfo
   | Unparsable UnparsableSourceInfo
@@ -53,51 +53,46 @@ type UnparsableSourceInfo = { line ∷ Int, reason ∷ String }
 
 component ∷ ∀ i m. MonadAff m ⇒ H.Component Query i Output m
 component = Hooks.component \{ outputToken, queryToken } _ → Hooks.do
-  _mbEditor /\ mbEditorId ← Hooks.useState Nothing
-  _sourceInfo /\ sourceInfoId ← Hooks.useState
-    $ Parsable { ast: Nil, formatted: "", unformatted: "" }
-
-  getMbEditor ← ExtraHooks.useGet _mbEditor
-  getSourceInfo ← ExtraHooks.useGet _sourceInfo
+  _mbState /\ _mbStateId ← Hooks.useState Nothing
+  getMbState ← ExtraHooks.useGet _mbState
 
   let
-    putMbEditor = Hooks.put mbEditorId
-    putSourceInfo = Hooks.put sourceInfoId
+    modifyMbState = Hooks.modify_ _mbStateId
 
-    inEditor ∷ ∀ a. (Editor → Effect a) → HookM m a
-    inEditor f = getMbEditor >>= liftEffect <<< case _ of
+    whenInitialized ∷ ∀ a. (State → HookM m a) → HookM m a
+    whenInitialized f = getMbState >>= case _ of
       Nothing →
-        throw "editor not initialized"
-      Just editor →
-        f editor
+        liftEffect $ throw "state not initialized"
+      Just state →
+        f state
 
     getSource ∷ HookM m String
-    getSource = inEditor Editor.getValue
+    getSource = whenInitialized \{ editor } →
+      liftEffect $ Editor.getValue editor
 
     updateSyntaxError ∷ Maybe UnparsableSourceInfo → HookM m Unit
-    updateSyntaxError mbUnparsableSourceInfo = inEditor \editor → do
-      info $ "updating syntax error: " <> show mbUnparsableSourceInfo
-      session ← Editor.getSession editor
-      case mbUnparsableSourceInfo of
-        Just { line, reason } →
-          Session.setAnnotations
-            [ { column: 0
-              , row: line - 1
-              , text: reason
-              , type: "error"
-              }
-            ]
-            session
-        Nothing →
-          Session.clearAnnotations session
+    updateSyntaxError mbUnparsableSourceInfo =
+      whenInitialized \{ editor } → liftEffect do
+        session ← Editor.getSession editor
+        case mbUnparsableSourceInfo of
+          Just { line, reason } →
+            Session.setAnnotations
+              [ { column: 0
+                , row: line - 1
+                , text: reason
+                , type: "error"
+                }
+              ]
+              session
+          Nothing →
+            Session.clearAnnotations session
 
     updateSource ∷ String → HookM m Unit
     updateSource newSource = do
       currentSource ← getSource
-      inEditor \editor → do
-        when
-          (newSource /= currentSource)
-          (void $ Editor.setValue newSource Nothing editor)
+      whenInitialized \{ editor } → liftEffect $ when
+        (newSource /= currentSource)
+        (void $ Editor.setValue newSource Nothing editor)
 
     handleChange ∷ HookM m Unit
     handleChange = do
@@ -107,32 +102,39 @@ component = Hooks.component \{ outputToken, queryToken } _ → Hooks.do
           let
             unparsableSourceInfo = { line, reason }
           updateSyntaxError (Just unparsableSourceInfo)
-          putSourceInfo $ Unparsable unparsableSourceInfo
+          modifyMbState $ map _
+            { sourceInfo = Unparsable unparsableSourceInfo }
           Hooks.raise outputToken SyntaxErrorDetected
 
         Right ast → do
           let
             formattedSource = formatSource ast
 
-          sourceInfo ← getSourceInfo
-          case sourceInfo of
-            Parsable info →
+          whenInitialized \{ sourceInfo } → case sourceInfo of
+            Parsable { ast: currentAst } →
               when
-                (ast /= info.ast)
+                {- TODO: fix
+                       this causes strange behaviour when the format
+                       button is clicked
+                -}
+                (ast /= currentAst || true)
                 (Hooks.raise outputToken (AstChanged { ast, source }))
             Unparsable _ → do
               updateSyntaxError Nothing
               Hooks.raise outputToken (AstChanged { ast, source })
 
-          putSourceInfo $ Parsable
-            { ast, formatted: formattedSource, unformatted: source }
+          modifyMbState $ map _
+            { sourceInfo = Parsable
+                { ast
+                , formatted: formattedSource
+                , unformatted: source
+                }
+            }
 
     handleFormatButtonClick ∷ HookM m Unit
-    handleFormatButtonClick = do
-      sourceInfo ← getSourceInfo
+    handleFormatButtonClick = whenInitialized \{ sourceInfo } →
       case sourceInfo of
-        Parsable { formatted } → do
-          info $ "updating source with formatted version: " <> formatted
+        Parsable { formatted } →
           updateSource formatted
         Unparsable _ →
           liftEffect $ throw "source cannot be formatted"
@@ -144,10 +146,25 @@ component = Hooks.component \{ outputToken, queryToken } _ → Hooks.do
       case mbAceElement of
         Just aceElement → do
           editor ← liftEffect $ Ace.editNode aceElement Ace.ace
-          session ← liftEffect $ Editor.getSession editor
-          putMbEditor $ Just editor
+          source ← liftEffect $ Editor.getValue editor
+          sourceInfo ← case Program.parseExpressions source of
+            Left (ParseError reason (Position { line })) → do
+              let
+                unparsableSourceInfo = { line, reason }
+              updateSyntaxError (Just unparsableSourceInfo)
+              pure $ Unparsable unparsableSourceInfo
+            Right ast →
+              pure $ Parsable
+                { ast
+                , formatted: formatSource ast
+                , unformatted: source
+                }
+
+          modifyMbState $ const $ Just { editor, sourceInfo }
+
           { emitter, listener } ← liftEffect HS.create
           subscriptionId ← Hooks.subscribe emitter
+          session ← liftEffect $ Editor.getSession editor
           liftEffect $ Session.onChange
             session
             (\_ → HS.notify listener cb)
@@ -162,11 +179,13 @@ component = Hooks.component \{ outputToken, queryToken } _ → Hooks.do
       [ HH.button
           [ HE.onClick \_ → handleFormatButtonClick
           , classes [ "button" ]
-          , HP.disabled case _sourceInfo of
-              Parsable { formatted, unformatted } →
+          , HP.disabled case _.sourceInfo <$> _mbState of
+              Just (Parsable { formatted, unformatted }) →
                 formatted == unformatted
-              Unparsable _ →
+              Just (Unparsable _) →
                 true
+              Nothing →
+                false
           ]
           [ HH.span
               [ classes [ "icon", "is-small" ] ]
@@ -188,7 +207,7 @@ component = Hooks.component \{ outputToken, queryToken } _ → Hooks.do
     subscriptionId ← subscribeToSessionChanges handleChange
 
     pure $ Just do
-      putMbEditor Nothing
+      modifyMbState $ const Nothing
       Hooks.unsubscribe subscriptionId
 
   Hooks.useQuery queryToken case _ of
