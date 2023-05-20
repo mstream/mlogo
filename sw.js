12a7367bc279386ff23e7e74ce8734581e3209ac
mstream <mstream@users.noreply.github.com>
2023-05-20 13:29:14 +0000

    deploy: 5dcc7e69230effc3fe802baa19125822122a3be9

---
+++ sw.js
@@ -0,0 +1,29 @@
+const cacheName = 'mlogo-0.39.0'
+
+const urlsToCache = [
+  '/',
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    cache.addAll(urlsToCache)
+  })())
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(cacheName)
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+      try {
+        const response = await fetch(event.request);
+        cache.put(event.request, response.clone());
+        return response;
+      } catch (e) { }
+    }
+  })())
+})
